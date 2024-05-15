8b276294b6fad610ab0be8aee3ecd23082db5edc
Barusha <110120000+Barusha@users.noreply.github.com>
2024-05-15 20:27:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ terraform {
 #Provider block
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIATPN6L5QCLBE24UO2"
- secret_key = "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"
+  access_key = ""
+ secret_key = ""
 }
